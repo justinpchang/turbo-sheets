b1d09994f41f7dd45269d1ac2d7d2b345f36f898
justinpchang <justin.p.chang@gmail.com>
2024-02-02 17:20:20 -0500

    Add sandboxing

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const { google } = require("googleapis");
 const express = require("express");
 const app = express();
 
+// TODO: (justinpchang) Replace with secure sandbox
+const { VM } = require("vm2");
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("API listening on port", port);
@@ -10,27 +13,24 @@ app.listen(port, () => {
 // Returns contents of A1 cell
 app.get("/:spreadsheetId", async (req, res) => {
   const spreadsheetId = req.params.spreadsheetId;
-  console.log("spreadsheetId", spreadsheetId);
-  const value = await getA1(spreadsheetId);
-  let retVal;
-  if (value) {
-    retVal = { status: "success", data: { value } };
-  } else {
-    res.status(404);
-    retVal = { status: "fail", data: { value } };
-  }
-  res.setHeader("content-type", "application/json");
-  res.end(JSON.stringify(retVal));
+  const vm = new VM({
+    sandbox: {
+      send: (data) => {
+        res.setHeader("content-type", "application/json");
+        res.end(JSON.stringify(data));
+      },
+      getA1: async function () {
+        const auth = await google.auth.getClient({
+          scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+        });
+        const api = google.sheets({ version: "v4", auth });
+        const response = await api.spreadsheets.values.get({
+          spreadsheetId,
+          range: "A1",
+        });
+        return response.data.values[0];
+      },
+    },
+  });
+  vm.run("getA1().then(send);");
 });
-
-async function getA1(spreadsheetId) {
-  const auth = await google.auth.getClient({
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-  });
-  const api = google.sheets({ version: "v4", auth });
-  const response = await api.spreadsheets.values.get({
-    spreadsheetId,
-    range: "A1",
-  });
-  return response.data.values[0];
-}
